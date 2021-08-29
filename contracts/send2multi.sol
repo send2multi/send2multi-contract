0c0ceec359bec5daa107b762a1ddfdadaeba984a
wangkui0508 <wangkui0508@hotmail.com>
2021-08-29 20:34:08 +0800

    Update send2multi.sol

--- contracts/send2multi.sol
+++ contracts/send2multi.sol
@@ -6,14 +6,14 @@ import "./IERC20.sol";
 contract Send2Multi {
 	function sendWithSameValue(address coinType, uint amount, address[] calldata receiverList) external {
 		for(uint i=0; i<receiverList.length; i++) {
-			IERC20(coinType).transfer(receiverList[i], amount);
+			IERC20(coinType).transferFrom(msg.sender, receiverList[i], amount);
 		}
 	}
 	function sendWithDifferentValues(address coinType, uint[] calldata receiverAndAmountList) external {
 		for(uint i=0; i<receiverAndAmountList.length; i++) {
 			uint96 amount = uint96(receiverAndAmountList[i]);
 			address receiver = address(bytes20(uint160(receiverAndAmountList[i]>>96)));
-			IERC20(coinType).transfer(receiver, uint(amount));
+			IERC20(coinType).transferFrom(msg.sender, receiver, uint(amount));
 		}
 	}
 }
