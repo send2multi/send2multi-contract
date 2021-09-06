9bf3cf255d6f40a57885630c17e9d32bb416c75d
wangkui0508 <wangkui0508@hotmail.com>
2021-09-06 23:34:03 +0800

    Switch to low-level calls

--- contracts/send2multi.sol
+++ contracts/send2multi.sol
@@ -6,14 +6,23 @@ import "./IERC20.sol";
 contract Send2Multi {
 	function sendWithSameValue(address coinType, uint amount, address[] calldata receiverList) external {
 		for(uint i=0; i<receiverList.length; i++) {
-			IERC20(coinType).transferFrom(msg.sender, receiverList[i], amount);
+			// We must use low-level call to avoid solidity add EXTCODEHASH checks
+			(bool success, bytes memory data) = coinType.call(
+			    abi.encodeWithSignature("transferFrom(address,address,uint256)", 
+						    msg.sender, receiverList[i], amount));
+			bool ok = abi.decode(data, (bool));
+			require(success && ok);
 		}
 	}
 	function sendWithDifferentValues(address coinType, uint[] calldata receiverAndAmountList) external {
 		for(uint i=0; i<receiverAndAmountList.length; i++) {
 			uint96 amount = uint96(receiverAndAmountList[i]);
 			address receiver = address(bytes20(uint160(receiverAndAmountList[i]>>96)));
-			IERC20(coinType).transferFrom(msg.sender, receiver, uint(amount));
+			(bool success, bytes memory data) = coinType.call(
+			    abi.encodeWithSignature("transferFrom(address,address,uint256)", 
+						    msg.sender, receiver, uint(amount)));
+			bool ok = abi.decode(data, (bool));
+			require(success && ok);
 		}
 	}
 }
